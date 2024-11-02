8e2a6b154940823a74f523639c5486c9eb9a9091
Raquelle M. Campos <raquellecampos12@gmail.com>
2024-11-01 21:36:17 -0400

    Update img.py

--- img.py
+++ img.py
@@ -2,13 +2,16 @@ import flet as ft
 
 def main(page: ft.Page):
     img = ft.Image(
-       # src='https://th.bing.com/th/id/OIP.5hIx18FWXLCeG5rKG8II9AHaHa?rs=1&pid=ImgDetMain',
-       src='/Users/raque/Documents/GitHub/Curso_Flet/assets/img.png',
+        # src='https://th.bing.com/th/id/OIP.5hIx18FWXLCeG5rKG8II9AHaHa?rs=1&pid=ImgDetMain',
+        src='assets/img/camera.jpg',
         border_radius= ft.border_radius.horizontal(left=10, right=10),
         height=500,
         width=500,
-        fit=ft.ImageFit.COVER,
+        fit=ft.ImageFit.CONTAIN,
+        tooltip='Canon EOS 100D',
     )
+
+    icon = ft.Icon
 
     page.add(img)
 
