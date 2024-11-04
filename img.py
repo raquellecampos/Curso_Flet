928bb96d0e676b05be03fb2d42ac5e7ac28d3cc7
Raquelle M. Campos <raquellecampos12@gmail.com>
2024-11-03 20:32:38 -0400

    Update img.py

--- img.py
+++ img.py
@@ -11,8 +11,39 @@ def main(page: ft.Page):
         tooltip='Canon EOS 100D',
     )
 
-    icon = ft.Icon
+    icon = ft.Icon(name=ft.icons.CAMERA, color=ft.colors.BLUE)
 
-    page.add(img)
+
+
+    btn1 = ft.ElevatedButton(text='Clique aqui', icon=ft.icons.CHECK)
+    btn2 = ft.ElevatedButton(text='Não clique aqui', icon=ft.icons.CANCEL, disabled=True)
+
+    estilo = ft.ButtonStyle(
+        color={
+            ft.MaterialState.HOVERED: ft.colors.WHITE,
+            ft.MaterialState.DEFAULT: ft.colors.BLACK,
+        },
+        bgcolor={
+            ft.MaterialState.HOVERED: ft.colors.PINK,
+            '': ft.colors.AMBER,
+        },
+        animation_duration=1000,
+
+    )
+
+    btn3 = ft.ElevatedButton(text='Botão com estilo personalizado', style=estilo)
+
+    page.add(img,icon, btn1, btn2, btn3)
+
+    def button_clicked(e):
+        e.control.data += 1
+        texto.value = f'Contagem: {e.control.data}'
+        texto.update()
+        btn.update()
+
+    btn = ft.ElevatedButton(text='Botão com contagem', icon=ft.icons.CABIN_SHARP, on_click=button_clicked, data=0)
+    texto = ft.Text(value='Contagem: 0', color=ft.colors.WHITE)
+
+    page.add(btn, texto)
 
 ft.app(target=main, assets_dir='assets')
